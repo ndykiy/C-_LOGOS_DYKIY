6b583bfed1ac29434340632709366704ab7342f3
Nazariy Dykiy <ndykiy@gmail.com>
2016-11-22 20:52:52 +0200

    homework

---
+++ HW_1_LOGOS/HW_1_LOGOS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_1_LOGOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nFirstMark;
+            int nSecondMark;
+            int nThirdMark;
+            Console.WriteLine("Enter 3 marks:");
+            nFirstMark = int.Parse(Console.ReadLine());
+            nSecondMark = int.Parse(Console.ReadLine());
+            nThirdMark = int.Parse(Console.ReadLine());
+            Console.WriteLine("The average mark is: {0}",(float)(nFirstMark+nSecondMark+nThirdMark)/3);
+        }
+    }
+}
---
+++ HW_2_LOGOS/HW_2_LOGOS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_LOGOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter radius (cm):");
+            float fRadius = (float.Parse)(Console.ReadLine());
+            Console.WriteLine("Length of the circle is {0} cm",2*fRadius*3.1415);
+        }
+    }
+}
