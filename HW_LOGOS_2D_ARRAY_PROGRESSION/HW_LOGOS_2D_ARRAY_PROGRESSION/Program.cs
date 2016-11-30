b3ee3a44a48331ffc4b3897564803d7511284a7b
Nazariy Dykiy <ndykiy@gmail.com>
2016-11-30 11:43:18 +0200

    homework

---
+++ HW_LOGOS_2D_ARRAY_PROGRESSION/HW_LOGOS_2D_ARRAY_PROGRESSION/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_LOGOS_2D_ARRAY_PROGRESSION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter dimensions of matrix");
+            int nW = int.Parse(Console.ReadLine());
+            int nH = int.Parse(Console.ReadLine());
+            int[,] nMatrix = new int[nW, nH];
+            FillMatrix(nMatrix, ref nW, ref nH);
+        }
+
+        static void FillMatrix(int[,] nArray,ref int nWidth, ref int nHeight)
+           {
+            
+            for(int i=0;i<nWidth;i++)
+            {
+                for (int j = 0; j < nHeight; j++)
+                {
+                    nArray[i, j] = i * j;
+                    Console.Write("   " + nArray[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+           }
+
+    }
+}
---
+++ HW_LOGOS_2D_ARRAY_PROGRESSION/HW_LOGOS_2D_ARRAY_PROGRESSION/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("HW_LOGOS_2D_ARRAY_PROGRESSION")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HW_LOGOS_2D_ARRAY_PROGRESSION")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2cbb889e-08fe-4077-b0c5-200fe732d681")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
