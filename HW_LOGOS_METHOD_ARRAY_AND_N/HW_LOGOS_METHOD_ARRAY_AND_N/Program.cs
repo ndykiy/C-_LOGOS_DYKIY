36397df459e6d2d6dbc0e32f1e906cb0f19e2602
Nazariy Dykiy <ndykiy@gmail.com>
2016-11-30 11:16:24 +0200

    homework

---
+++ HW_LOGOS_METHOD_ARRAY_AND_N/HW_LOGOS_METHOD_ARRAY_AND_N/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_LOGOS_METHOD_ARRAY_AND_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int nNum = ReturnPosition();
+            Console.WriteLine(nNum);
+
+        }
+
+        static int ReturnPosition()
+        {
+            Console.WriteLine("Enter quantity of array");
+            int nQuantity = int.Parse(Console.ReadLine());
+            int[] nArray = new int[nQuantity];
+            Console.WriteLine("Enter number to look for");
+            int nNumber = int.Parse(Console.ReadLine());
+            Random rand = new Random();
+            for (int i=0;i<nQuantity;i++)
+            {
+                nArray[i] = rand.Next(1, 9);
+                Console.Write(" "+nArray[i]);
+            }
+            Console.WriteLine();
+            int j=-1;
+            //bool bFound;
+            for (int i=0;i<nQuantity;i++)
+            {
+                if (nArray[i] == nNumber)
+                    j = i;
+            }
+            return j;
+        }
+    }
+}
---
+++ HW_LOGOS_STRING_PARAMS/HW_LOGOS_STRING_PARAMS/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_LOGOS_STRING_PARAMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string");
+            string sStrichka = Console.ReadLine();
+
+            Console.WriteLine("Enter quantity of array");
+            int nQ = int.Parse(Console.ReadLine());
+            Random rand = new Random();
+            int[] nArr = new int[nQ];
+            for (int i=0;i<nQ;i++)
+            {
+                nArr[i] = rand.Next(1, 9);
+                Console.Write(" "+nArr[i]);
+
+            }
+            PrintResult(sStrichka,nArr);
+        }
+
+        static void PrintResult(string sString, params int[] nArray)
+        {
+            int nSum = 0;
+            for (int i=0;i<nArray.Length;i++)
+            {
+                nSum = nSum + nArray[i];
+            }
+            if(nSum>20)
+            {
+                Console.WriteLine(sString);
+            }
+            else
+            {
+                Console.WriteLine("Sorry, but sum is {0}", nSum);
+            }
+;
+        }
+
+    }
+}
