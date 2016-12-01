cb11a9ab8ef71efae964a84ad91b5af23db4a849
Nazariy Dykiy <ndykiy@gmail.com>
2016-12-01 20:12:54 +0200

    test

--- LOGOS_LESSON_METHODS/LOGOS_LESSON_METHODS/Program.cs
+++ LOGOS_LESSON_METHODS/LOGOS_LESSON_METHODS/Program.cs
@@ -64,13 +64,12 @@ namespace LOGOS_LESSON_METHODS
                 nSum = nSum + A[i];
             }
             return nSum;
-            */
+            
 
             Console.WriteLine("Enter quantity of array");
             int nQ = int.Parse(Console.ReadLine());
-            int[] nArr = new int[nQ];
-            
-                nArr = NewArray(nQ);
+            int[] nArr;
+            nArr = NewArray(nQ);
             for(int i=0;i<nQ;i++)
             {
                 Console.Write(" {0}", nArr[i]);
@@ -87,10 +86,88 @@ namespace LOGOS_LESSON_METHODS
             for (int i = 0; i < nQuant; i++)
             {
                 nA[i] = i;
-               // Console.Write(" {0}",nA[i]);
             }
             return nA;
+            
+
+            Figure(3,4,6);
+
+        }
+
+        static void Figure(int n1)
+        {
+            Console.WriteLine("Line with Length {0}", n1);
+        }
+
+        static void Figure(int n1,int n2)
+        {
+            Console.WriteLine("Dot with coordinates {0},{1}", n1,n2);
+        }
+
+        static void Figure(int n1,int n2,int n3)
+        {
+            Console.WriteLine("Triangle with sides {0},{1},{2}", n1, n2,n3);
+        }
+
+        static void Figure(int n1, int n2, int n3,int n4)
+        {
+            Console.WriteLine("Rectangle with sides {0},{1},{2},{3}", n1, n2, n3,n4);
+        }
+        
+            Figure(3,3 ,5,6);
+        }
+
+        static void Figure(int n1, int n2=0, int n3=0,  int n4=0)
+        {
+
+
+            if(n4==0&&n3!=0&&n2!=0)
+            {
+                Console.WriteLine("Triangle with sides {0},{1},{2}", n1, n2, n3);
+            }
+            else
+            {
+                if(n3==0&&n2!=0)
+                    Console.WriteLine("Dot with coordinates {0},{1}", n1, n2);
+                else
+                {
+                    if(n2==0)
+                        Console.WriteLine("Line with Length {0}", n1);
+                    else
+                    {
+                        Console.WriteLine("Rectangle with sides {0},{1},{2},{3}", n1, n2, n3, n4);
+                    }
+                }
+            }
+            */
+            Console.WriteLine("Enter quantity of numbers");
+            int nQ = int.Parse(Console.ReadLine());
+            int[] a = new int[nQ];
+            Random rand = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rand.Next(1, 9);
+                Console.Write(" {0}" , a[i]);
+            }
+
+                int nmin, nmax;
+            MinMax(a,out nmin,out nmax);
+            Console.WriteLine();
+            Console.WriteLine(nmin + " " + nmax);
+        }
+
+        static void MinMax(int[] arr,out int min,out int max)
+        {
+             min = arr[0];
+             max = arr[0];
+            for(int i=0;i<arr.Length;i++)
+            {
+                if (arr[i] < min)
+                    min = arr[i];
+                if (arr[i] > max)
+                    max = arr[i];
+            }
         }
 
     }
-}
+    }
---
+++ LOGOS_TEST/LOGOS_TEST/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOGOS_TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            string sQ1 = "Enter course, we attend";
+            string[] sA1 = new string[3];
+            sA1[0] = "C#";
+            sA1[1] = "html css";
+            sA1[2] = "sql db";
+            
+
+            string sQ2 = "Enter today's month";
+            string[] sA2 = new string[3];
+            sA2[0] = "august";
+            sA2[1] = "december";
+            sA2[2] = "half moon";
+            
+            string sQ3 = "Enter today's day of week";
+            string[] sA3 = new string[3];
+            sA3[0] = "monday";
+            sA3[1] = "tuesday";
+            sA3[2] = "thursday";
+            
+            string sQ4 = "Enter today's year";
+            string[] sA4 = new string[3];
+            sA4[0] = "2007";
+            sA4[1] = "1415";
+            sA4[2] = "2016";
+
+            int[] aA = new int[4];
+            aA[0] = 1;
+            aA[1] = 2;
+            aA[2] = 3;
+            aA[3] = 3;
+            int[] a = new int[4];
+            int nResult = 0;
+            Question(sQ1, sA1);
+            a[0] = int.Parse(Console.ReadLine());
+            if (a[0] == aA[0])
+                nResult++;
+
+            Question(sQ2, sA2);
+
+            a[1] = int.Parse(Console.ReadLine());
+            if (a[1] == aA[1])
+                nResult++;
+
+            Question(sQ3, sA3);
+            a[2] = int.Parse(Console.ReadLine());
+            if (a[2] == aA[2])
+                nResult++;
+
+            Question(sQ4, sA4);
+            a[3] = int.Parse(Console.ReadLine());
+            if (a[3] == aA[3])
+                nResult++;
+
+
+            Console.WriteLine("Your result is " + nResult);
+
+
+        }
+
+        static void Question(string sQuestion,params string[] sAnswer)
+        {
+           
+           
+            Console.WriteLine(sQuestion);
+            for (int i=0;i<3;i++)
+            {
+                Console.WriteLine((i+1)+sAnswer[i]);
+                
+            }
+
+            
+
+
+
+           
+        }
+
+    }
+}
