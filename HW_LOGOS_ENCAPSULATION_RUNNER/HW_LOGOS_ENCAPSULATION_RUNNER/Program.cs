7282c8b4ab15871768e4d928f77876fae8f68bd0
Nazariy Dykiy <ndykiy@gmail.com>
2016-12-07 23:13:00 +0200

    hw

--- HW_LOGOS_ENCAPSULATION_RUNNER/HW_LOGOS_ENCAPSULATION_RUNNER/Program.cs
+++ HW_LOGOS_ENCAPSULATION_RUNNER/HW_LOGOS_ENCAPSULATION_RUNNER/Program.cs
@@ -10,6 +10,28 @@ namespace HW_LOGOS_ENCAPSULATION_RUNNER
     {
         static void Main(string[] args)
         {
+            Runner[] runner = new Runner[2];
+
+
+            runner[0] = new Runner() ;
+           
+            runner[1] = new Runner() ;
+
+            runner[0].Print();
+            runner[1].Print();
+
+
+            int nMin = runner[0].time;
+            for(int i=0;i<2;i++)
+            {
+                if(runner[i].time<nMin)
+                {
+                    nMin = runner[i].time;
+                }
+            }
+
+            Console.WriteLine("The fastest is {0}", nMin);
+
         }
     }
 }
--- HW_LOGOS_ENCAPSULATION_RUNNER/HW_LOGOS_ENCAPSULATION_RUNNER/Runner.cs
+++ HW_LOGOS_ENCAPSULATION_RUNNER/HW_LOGOS_ENCAPSULATION_RUNNER/Runner.cs
@@ -8,5 +8,75 @@ namespace HW_LOGOS_ENCAPSULATION_RUNNER
 {
     class Runner
     {
+        private string sName;
+        private int nTime;
+
+        private static readonly Random getrandom = new Random();
+        private static readonly object syncLock = new object();
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (syncLock)
+            { // synchronize
+                return getrandom.Next(min, max);
+            }
+        }
+
+        public Runner()
+        {
+            nTime = GenerateTime();
+            sName = GenerateName();
+        }
+
+        public string name
+        {
+            get
+            {
+                return sName;
+            }
+            set
+            {
+                sName= GenerateName();
+            }
+        }
+
+        public static  string GenerateName()
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            int size = 10;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Runner.GetRandomNumber(0,26) + 65));
+                builder.Append(ch);
+            }
+            return builder.ToString();
+        }
+
+
+        public int time
+        {
+            get
+            {
+                return nTime;
+            }
+            set
+            {
+                nTime = GenerateTime();
+            }
+        }
+
+        public static int GenerateTime()
+        {
+            Random rand = new Random();
+            return Runner.GetRandomNumber(1, 15);
+       
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("{0} {1}", sName, nTime);
+        }
+
     }
 }
