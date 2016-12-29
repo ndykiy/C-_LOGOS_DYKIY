671e0b624842b4c1298da4bf0754c2f6a6bf5ec3
Nazariy Dykiy <ndykiy@gmail.com>
2016-12-29 18:28:44 +0200

    hw

---
+++ HW_LOGOS_VECTOR/HW_LOGOS_VECTOR/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_LOGOS_VECTOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector[] vector = new Vector[10];
+            for(int i=0;i<10;i++)
+            {
+                vector[i] = new Vector();
+                vector[i].SetX();
+                vector[i].SetY();
+            }
+
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine("Length of {0} vector is {1}", i + 1, vector[i].SetL());
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                if(vector[i].SetL()>5)
+                Console.WriteLine("Length of {0} vector is {1}", i + 1, vector[i].SetL());
+            }
+
+        }
+    }
+}
---
+++ HW_LOGOS_VECTOR/HW_LOGOS_VECTOR/Vector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_LOGOS_VECTOR
+{
+    class Vector
+    {
+        public int x;
+        public int y;
+
+        public void    SetX()
+        {
+            Console.WriteLine("Enter x");
+            x =int.Parse(Console.ReadLine());
+        }
+
+        public void  SetY()
+        {
+            Console.WriteLine("Enter y");
+          y=  int.Parse(Console.ReadLine());
+        }
+
+
+        public double SetL()
+        {
+            return Math.Sqrt(x*x+y*y);
+        }
+      
+    }
+}
