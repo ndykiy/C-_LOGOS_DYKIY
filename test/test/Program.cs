3869093a20d33f02ec8584e21fc6ed18b65d2150
Nazariy Dykiy <ndykiy@gmail.com>
2016-12-29 19:52:05 +0200

    test

---
+++ test/test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestStack test = new TestStack();
+            Console.WriteLine("Enter line");
+            test.CheckLine(Console.ReadLine());
+        }
+        
+        
+       
+    }
+}
---
+++ test/test/TestStack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class TestStack
+    {
+        public void CheckLine(string s)
+        {
+            Stack<string> sLine = new Stack<string>();
+            string[] split = s.Split(new Char[] { ' ' });
+            for(int i=0;i<split.Length;i++)
+            {
+                sLine.Push(split[i]);
+            }
+
+            Console.WriteLine("Elements in sentence:\n");
+
+            for(int i = 0; i < split.Length; i++)
+            {
+                Console.WriteLine(sLine.Peek());
+                sLine.Pop();
+            }
+
+        }
+        
+
+
+    }
+}
